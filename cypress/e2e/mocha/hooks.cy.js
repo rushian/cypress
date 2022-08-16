4ba22210de305184c92d80d9bd2da04c6f5d389b
Luciano <luciano.ivec@gmail.com>
2022-08-15 21:24:09 -0300

    aplicacao do lint

--- cypress/e2e/mocha/hooks.cy.js
+++ cypress/e2e/mocha/hooks.cy.js
@@ -1,11 +1,11 @@
-describe("testes do site iterasys", () => {
-  //eu crio testes em cypress com o it
-  it("primeiro teste", () => {
-    //cy.visit('www.iterasys.com.br');
-    cy.log("Meu primeiro teste");
-  });
+describe('testes do site iterasys', () => {
+    // eu crio testes em cypress com o it
+    it('primeiro teste', () => {
+    // cy.visit('www.iterasys.com.br');
+        cy.log('Meu primeiro teste');
+    });
 
-  it("segundo test", () => {
-    cy.log("Meu segundo teste");
-  });
+    it.only('segundo test', () => {
+        cy.log('Meu segundo teste');
+    });
 });
